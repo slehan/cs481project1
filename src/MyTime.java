61c36cb442c0eacef333bddab2fa42225d07adff
Skyler Lehan <slehan117@icloud.com>
2016-02-11 11:45:43 -0500

    Completed MyTime.java

--- src/MyTime.java
+++ src/MyTime.java
@@ -1,20 +1,31 @@
 
 
-import java.util.*;
-import java.text.*;
-
-import java.util.Date;
 /* Command:
            time
            Action:
            the current time is displayed.
-  */
+ */
+import java.util.*;
 
 public class MyTime{
-  public MyTime(){
-    f();
-  }
+	public MyTime(){
+		f();
+	}
 
-  private void f(){
-  }
+	private void f(){
+
+		// Create a calendar to get the date
+		TimeZone tz = TimeZone.getTimeZone("America/New_York");
+		GregorianCalendar cal = new GregorianCalendar(tz);
+		
+		// Example: 11:36:33 EST
+		// Get hours, minutes, seconds and time zone letters
+		int hours = cal.get(Calendar.HOUR_OF_DAY);
+		int minutes = cal.get(Calendar.MINUTE);
+		int seconds = cal.get(Calendar.SECOND);
+		String tzStr = tz.getDisplayName();
+		
+		// Print it out
+		System.out.format("%d:%d:%02d " + tzStr + "\n", hours, minutes, seconds);
+	}
 }
